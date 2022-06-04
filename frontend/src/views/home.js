9f6803fc4e1f3978f2e058ee325e243bd571b2d6
arghya17 <arghyadeepsaha17@gmail.com>
2022-06-04 12:32:11 +0530

    Working version of auth0 added

---
+++ frontend/src/views/home.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+
+import Hero from "../components/hero";
+import HomeContent from "../components/home-content";
+
+const Home = () => (
+  <Fragment>
+    <Hero />
+    <hr />
+    <HomeContent />
+  </Fragment>
+);
+
+export default Home;
